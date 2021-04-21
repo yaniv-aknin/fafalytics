02a54d5e7aa84fbdf9e4993c9be82d071572ced4
Yaniv Aknin <yaniv@aknin.name>
2021-04-21 18:21:14 +0100

    add csv/parquet support

--- fafalytics/exports.py
+++ fafalytics/exports.py
@@ -6,9 +6,14 @@ import pandas as pd
 from .datastore import get_client
 
 @click.command()
-@click.argument('output', type=click.File(mode='w'))
-def export(output):
+@click.option('--format', type=click.Choice(['parquet', 'csv']))
+@click.argument('output', type=click.Path(dir_okay=False, writable=True))
+def export(format, output):
     client = get_client()
     game_ids = client.keys('game.*')
-    df = pd.json_normalize([json.loads(game) for game in client.mget(game_ids)])
-    df.set_index('id').to_csv(output)
+    df = pd.json_normalize([json.loads(game) for game in client.mget(game_ids)]).set_index('id')
+    format = format or 'csv' if output.endswith('csv') else 'parquet'
+    if format == 'csv':
+        df.to_csv(output)
+    else:
+        df.to_parquet(output)

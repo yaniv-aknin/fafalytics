63f3eac0ae017b4d4e1875f7caca6c6fcad30f98
Yaniv Aknin <yaniv@aknin.name>
2021-04-25 21:45:03 +0100

    runcommands: add a few convenience functions

--- runcommands.sh
+++ runcommands.sh
@@ -1,3 +1,14 @@
 # source this file from bin/activate of your venv
 export FAFAL_LOGLEVEL=info
 alias redis='redis-cli -s /tmp/.fafalytics.d/redis.sock'
+function newfafa {
+    local data_dir=../data-dumps/dump-01
+    [ -d $data_dir ] || { echo missing $data_dir ; return 1 ; }
+    fafalytics datastore restart
+    fafalytics load $data_dir/jsons/*
+    fafalytics extract --max-errors 0 $data_dir/unpacked/*
+    fafalytics export /tmp/test.parquet curated
+}
+function pqfafa {
+    ipython -c 'import pandas as pd ; df=pd.read_parquet("/tmp/test.parquet")' -i
+}

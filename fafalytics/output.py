0bd4e4bf484ac573c61f076b97473f377a380b81
Yaniv Aknin <yaniv@aknin.name>
2021-04-27 08:09:04 +0100

    output: simplify and make iterative
    This loses the 'console' output, but it wasn't too useful anyway.

--- fafalytics/output.py
+++ fafalytics/output.py
@@ -4,14 +4,13 @@ import json
 import click
 
 from .storage import get_client
-from .pyutils import shell as python_shell
 
 def yields_outputs(func):
     @click.option('--output', type=click.Choice(tuple(OUTPUT_CALLBACKS)), default='datastore')
     @functools.wraps(func)
     def wrapper(output, *args, **kwargs):
-        objects = [obj for obj in func(output, *args, **kwargs)]
-        OUTPUT_CALLBACKS[output](func.__name__, objects)
+        for obj in func(output, *args, **kwargs):
+            OUTPUT_CALLBACKS[output](func.__name__, obj)
     return wrapper
 
 
@@ -21,12 +20,5 @@ def output(func):
     return func
 
 @output
-def datastore(prefix, objects):
-    client = get_client()
-    with click.progressbar(objects, label="Outputting") as bar:
-        for obj in bar:
-            client.hsetnx(prefix, obj['id'], json.dumps(obj))
-
-@output
-def console(prefix, objects):
-    python_shell({'objects': objects})
+def datastore(prefix, obj):
+    get_client().hsetnx(prefix, obj['id'], json.dumps(obj))

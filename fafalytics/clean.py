8c905b8a8d27f19542b5998eb230204a7cb88e4f
Yaniv Aknin <yaniv@aknin.name>
2021-05-02 21:36:04 +0100

    clean: separate cleaning logic to own module
    I hate to tell you why I'm doing this.
    
    Hint:
    `exec(requests.get('github.com/...clean.py').content, globals(), locals()`
    ...way shorter than `pip install fafalytics` in Colab *shrug*

---
+++ fafalytics/clean.py
@@ -0,0 +1,27 @@
+import datetime
+import pandas as pd
+
+THREE_MINUTES_IN_TICKS = 10*60*3
+SIX_HOURS_IN_TICKS = 10*60*60*6
+GAMES_AFTER = datetime.datetime(2005, 1, 1)
+GAMES_BEFORE = datetime.datetime(2025, 1, 1)
+
+def clean_dataframe(df, min_duration=THREE_MINUTES_IN_TICKS, max_duration=SIX_HOURS_IN_TICKS, min_rating=-250, max_rating=3000, games_after=GAMES_AFTER, games_before=GAMES_BEFORE):
+    # add convenience values
+    for prefix in ('player1.player.', 'player2.player.'):
+        df[prefix + 'faf_rating.before'] = df[prefix + 'trueskill_mean_before'] - 3 * df[prefix + 'trueskill_deviation_before']
+    df['meta.rating_delta'] = (df['player1.player.faf_rating.before']-df['player2.player.faf_rating.before']).abs().astype('int32')
+    df['meta.rating_mean'] = ((df['player1.player.faf_rating.before']+df['player2.player.faf_rating.before'])/2).astype('int32')
+    df['meta.duration'] = pd.to_timedelta(df['durations.ticks'] * 10, unit='seconds')
+
+    # filter unreasonable values
+    return df[
+        (df['durations.database.start'] > games_after) &
+        (df['durations.database.start'] < games_before) &
+        (df['durations.ticks'] > min_duration) &
+        (df['durations.ticks'] < max_duration) &
+        (df['player1.player.faf_rating.before'] > min_rating) &
+        (df['player1.player.faf_rating.before'] < max_rating) &
+        (df['player2.player.faf_rating.before'] > min_rating) &
+        (df['player2.player.faf_rating.before'] < max_rating)
+    ]
--- fafalytics/manual.py
+++ fafalytics/manual.py
@@ -5,6 +5,7 @@ from .parsing import get_parsed
 from .manyfiles import process_all_files
 from .pyutils import shell as python_shell
 from .pdutils import patch_dataframe_with_query_columns
+from .clean import clean_dataframe
 
 @click.group()
 def manual():
@@ -31,25 +32,6 @@ def dataframe(infile):
 def clean(infile, outfile):
     df = pd.read_pickle(infile)
     old_shape = df.shape
-
-    # add convenience values
-    for prefix in ('player1.player.', 'player2.player.'):
-        df[prefix + 'faf_rating.before'] = df[prefix + 'trueskill_mean_before'] - 3 * df[prefix + 'trueskill_deviation_before']
-    df['meta.rating_delta'] = (df['player1.player.faf_rating.before']-df['player2.player.faf_rating.before']).abs().astype('int32')
-    df['meta.rating_mean'] = ((df['player1.player.faf_rating.before']+df['player2.player.faf_rating.before'])/2).astype('int32')
-    df['meta.duration'] = pd.to_timedelta(df['durations.ticks'] * 10, unit='seconds')
-
-    # filter unreasonable values
-    THREE_MINUTES_IN_TICKS = 10*60*3
-    SIX_HOURS_IN_TICKS = 10*60*60*6
-    df = df[
-        (df['durations.ticks'] > THREE_MINUTES_IN_TICKS) &
-        (df['durations.ticks'] < SIX_HOURS_IN_TICKS) &
-        (df['player1.player.faf_rating.before'] > -250) &
-        (df['player1.player.faf_rating.before'] < 3000) &
-        (df['player2.player.faf_rating.before'] > -250) &
-        (df['player2.player.faf_rating.before'] < 3000)
-    ]
-
+    df = clean_dataframe(df)
     df.to_pickle(outfile)
     print('Cleaned df %s to %s' % (old_shape, df.shape))

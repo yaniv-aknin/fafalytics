ff991d5043a6ec3f205d4f1c16e3829f804f3be5
Yaniv Aknin <yaniv@aknin.name>
2021-05-04 06:46:53 +0100

    clean: fix tests, make less fragile

--- tests/test_clean.py
+++ tests/test_clean.py
@@ -11,8 +11,10 @@ from fafalytics.clean import clean_dataframe
 def test_clean(df):
     assert df.shape == (200, 129)
     cdf = clean_dataframe(df)
-    assert cdf.shape == (182, 134)
+    assert cdf.shape[0] == 182
+    assert cdf.shape[1] > 129
     assert cdf.qc.meta.duration.max().seconds == 4077
+    assert cdf.qc.meta.rating.mean.max() == 2029
 
 @pytest.fixture
 def df():

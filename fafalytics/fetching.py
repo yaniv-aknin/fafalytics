89058b69c1cc20b91ae0ca761ac3e67b0f8ab748
Yaniv Aknin <yaniv@aknin.name>
2021-05-09 08:28:16 +0100

    fetch: add sort, simplify start/end date

--- fafalytics/fetching.py
+++ fafalytics/fetching.py
@@ -15,18 +15,20 @@ API_BASE = urlobject.URLObject('https://api.faforever.com')
 def isoformat(date):
     return datetime.datetime.combine(date, datetime.time()).isoformat(timespec='seconds') + 'Z'
 
-def build_url(api_base, page_size, max_pages, page_number, start_date, duration_weeks):
+def build_url(api_base, page_size, max_pages, page_number, start_date, end_date, sort='ASC'):
     url = api_base.add_path('/data/game')
     url = url.add_query_param('page[size]', page_size)
     url = url.add_query_param('page[number]', page_number)
     url = url.add_query_param('page[totals]', '')
-    start_formatted = isoformat(start_date)
-    end_formatted = isoformat(start_date + duration_weeks)
-    filter_param = ('((playerStats.ratingChanges.leaderboard.id=in=("2");endTime=ge="%s";'
-                    'endTime=le="%s";validity=in=("VALID")));endTime=isnull=false' % (start_formatted, end_formatted))
-    url = url.add_query_param('filter', filter_param)
+    filter_param = ['((playerStats.ratingChanges.leaderboard.id=in=("2");validity=in=("VALID"))']
+    if start_date:
+        filter_param.append(';endTime=ge="%s"' % isoformat(start_date))
+    if end_date:
+        filter_param.append(';endTime=le="%s"' % isoformat(end_date))
+    filter_param.append(');endTime=isnull=false')
+    url = url.add_query_param('filter', "".join(filter_param))
     url = url.add_query_param('include', 'playerStats,playerStats.player,mapVersion,mapVersion.map')
-    url = url.add_query_param('sort', '-startTime')
+    url = url.add_query_param('sort', '-startTime' if sort == 'DESC' else 'startTime')
     return url
 
 def write_response(directory, index, obj):
@@ -41,14 +43,15 @@ def fetch():
 @click.option('--api-base', type=urlobject.URLObject, default=API_BASE)
 @click.option('--page-size', type=int, default=10)
 @click.option('--max-pages', type=int)
-@click.option('--start-date', type=click.DateTime(['%Y-%m-%d']), default='2021-01-01')
-@click.option('--duration-weeks', type=lambda x: datetime.timedelta(weeks=int(x)), default=12)
+@click.option('--start-date', type=click.DateTime(['%Y-%m-%d']), default=None)
+@click.option('--end-date', type=click.DateTime(['%Y-%m-%d']), default=None)
 @click.option('--sleep-interval', type=float, default=10)
 @click.option('--dry-run/--no-dry-run', default=False)
+@click.option('--sort', type=click.Choice(['ASC', 'DESC']), default='ASC')
 @click.argument('output_directory', type=click.Path())
-def games(api_base, page_size, max_pages, start_date, duration_weeks, sleep_interval, dry_run, output_directory):
+def games(api_base, page_size, max_pages, start_date, end_date, sleep_interval, dry_run, sort, output_directory):
     "Get JSON dumps of Game model objects from api.faforever.com"
-    first_url = build_url(api_base, page_size, max_pages, 1, start_date, duration_weeks)
+    first_url = build_url(api_base, page_size, max_pages, 1, start_date, end_date, sort)
     print('Fetching 1st page...')
     first_response = requests.get(first_url).json()
     if first_response['meta']['page']['totalRecords'] == 0:
@@ -63,7 +66,7 @@ def games(api_base, page_size, max_pages, start_date, duration_weeks, sleep_inte
             if max_pages and page_number > max_pages:
                 break
             time.sleep(sleep_interval)
-            url = build_url(api_base, page_size, max_pages, page_number, start_date, duration_weeks)
+            url = build_url(api_base, page_size, max_pages, page_number, start_date, end_date, sort)
             if dry_run:
                 continue
             write_response(output_directory, page_number, requests.get(url).json())
